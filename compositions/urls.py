f8d13f43021a645de842dffd5c5ae1f74d620be3
fkalebic <fkalebic@gmail.com>
2017-09-06 13:20:03 +0200

    login form

--- compositions/urls.py
+++ compositions/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.user_login, name='index'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^welcome/$', views.welcome, name='welcome'),
+    url(r'^grade/$', views.grade, name='grade'),
 ]
